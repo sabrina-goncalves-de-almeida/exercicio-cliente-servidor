21680fbb1f76968f69c237fb20af7cbe430d6cb8
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2023-10-16 22:32:25 -0300

    first commit

---
+++ python/cliente.py
@@ -0,0 +1,28 @@
+import requests
+
+SERVIDOR_URL = 'http://127.0.0.1:5555'
+
+def enviar_mensagem():
+    msg = input("Digite sua mensagem: ")
+    resposta = requests.post(f'{SERVIDOR_URL}/enviar', json={'mensagem': msg})
+    print(resposta.json()['status'])
+
+def receber_mensagens():
+    resposta = requests.get(f'{SERVIDOR_URL}/receber')
+    mensagens = resposta.json()['mensagens']
+    print("Mensagens recebidas:")
+    for msg in mensagens:
+        print(f"- {msg}")
+
+if __name__ == '__main__':
+    while True:
+        print("1. Enviar mensagem")
+        print("2. Receber mensagens")
+        escolha = input("Escolha uma opção: ")
+        
+        if escolha == '1':
+            enviar_mensagem()
+        elif escolha == '2':
+            receber_mensagens()
+        else:
+            print("Opção inválida.")
---
+++ python/servidor.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+mensagens = []
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    msg = request.json.get('mensagem')
+    if msg:
+        mensagens.append(msg)
+        return jsonify({'status': 'Mensagem enviada'}), 200
+    return jsonify({'status': 'Erro ao enviar mensagem'}), 400
+
+@app.route('/receber', methods=['GET'])
+def receber():
+    return jsonify({'mensagens': mensagens})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
