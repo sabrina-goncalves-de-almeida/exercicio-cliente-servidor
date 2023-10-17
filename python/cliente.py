f5f05839f44519961fee4944e4c93a1332d254e6
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2023-10-17 00:25:38 -0300

    feat(#1): utilizando a biblioteca rich para estilizar a exibição no terminal

--- python/cliente.py
+++ python/cliente.py
@@ -3,6 +3,12 @@
 # - Após enviar a mensagem automaticamente limpar a tela e exibir toda a conversa novamente
 
 import requests
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.text import Text
+
+console = Console()
+console.print("Terminal", "Root", style="#ccc010 bold")
 
 SERVIDOR_URL = 'http://127.0.0.1:5555'
 
@@ -18,19 +24,31 @@ def receber_mensagens():
     for msg in mensagens:
         print(f"- {msg}")
 
+def texto_estilizado():
+    text = Text("Escolha uma opção")
+    text.stylize("bold white on #054f77")
+    return text
+
 if __name__ == '__main__':
+    console.rule("[bold white]CHAT PYTHON")
+    print("\n")
     while True:
-        print("1. Enviar mensagem")
-        print("2. Receber mensagens")
-        print("3. Sair")
-        escolha = input("Escolha uma opção: ")
+        console.print("1. Enviar mensagem", style="bold white on #054f77", justify="center")
+        console.print("2. Receber mensagens", style="bold white on #054f77", justify="center")
+        console.print("3. Sair", style="bold white on #054f77", justify="center")
+        print("\n\n")
+        escolha = Prompt.ask(texto_estilizado())
         
         if escolha == '1':
             enviar_mensagem()
         elif escolha == '2':
             receber_mensagens()
         elif escolha == '3':
-            print("Até mais, tenha um ótimo dia.")
+            print("\n")
+            console.rule("[bold white]Até mais, tenha um ótimo dia.")
+            print("\n")
             break
         else:
-            print("Opção inválida.")
+            print("\n")
+            console.print("Opção inválida.", style="bold #ff0000", justify="center")
+            print("\n")
